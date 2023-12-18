07bca4ccedbd8c799b44a0ba7384a9b3d1d8c774
0liveirax <150380352+0liveirax@users.noreply.github.com>
2023-12-17 23:06:04 -0300

    Add files via upload

---
+++ Projeto3/src/projetoCriptografia/CriptografiaMain.java
@@ -0,0 +1,42 @@
+package projetoCriptografia;
+
+import javax.swing.JOptionPane;
+
+public class CriptografiaMain {   
+    
+    public static int ASCII;
+    public static String chaveCrip = new String();
+    private static String codDecod = new String();
+    
+    public static void main(String[] args) {
+               
+        String chave = new String();
+        chave = JOptionPane.showInputDialog("Insira um texto");
+        criptografador(chave); 
+        descriptografar(chaveCrip);
+        
+    }
+    public static void criptografador(String chave){                               
+        char aux ;  
+        
+        for(int i = 0 ; i <= chave.length()-1 ; i++){
+             ASCII = (char) chave.charAt(i)-4 * i;
+             aux = (char)ASCII ;             
+             chaveCrip += Character.toString(aux);
+        }           
+        JOptionPane.showMessageDialog(null, chaveCrip);
+    }
+    
+    public static void descriptografar (String chave){
+        char c;		
+
+        for (int d = 0; d <= chave.length()-1; d++){	
+			
+            ASCII = chaveCrip.charAt(d)+4 * d;
+            c = (char)ASCII;
+            codDecod += Character.toString(c);			
+        }
+        JOptionPane.showMessageDialog(null,codDecod); 
+    }	
+    
+}
